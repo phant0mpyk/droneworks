d7f3ef249bc8cd4fa58ba0dea4584ed4e1663b07
Martin Matloha <uino45@gmail.com>
2026-06-08 20:21:23 +0200

    Made additions to scriptable object and adjusted hover rpm percentage.

--- Assets/Scripts/Martins' scripts/AudioSFXScriptableObject.cs
+++ Assets/Scripts/Martins' scripts/AudioSFXScriptableObject.cs
@@ -5,14 +5,20 @@ public class AudioClipEntry
 {
     public bool repeatable;
     public string clipName;
+    public string englishSpeakerName;
+    public string dutchSpeakerName;
     public string englishSubtitles;
     public string dutchSubtitles;
     public AudioClip audioClip;
+    public AudioClip audioClipAlternative;
     public float timeToPlayTimedClipSeconds;
     public string timedClipName;
+    public string timedEnglishSpeakerName;
+    public string timedDutchSpeakerName;
     public string timedEnglishSubtitles;
     public string timedDutchSubtitles;
     public AudioClip audioTimedClip;
+    public AudioClip audioTimedClipAlternative;
 }
 
 [CreateAssetMenu(fileName = "AudioSFXScriptableObject", menuName = "Scriptable Objects/AudioSFXScriptableObject")]
